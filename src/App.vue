682fbe54f324c9a7bfe4decea9fff01fe4bb92a0
Backendpapa <98647710+backendpapa@users.noreply.github.com>
2022-01-30 21:27:53 +0100

    initial disney

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,9 @@
   </div>
       <v-spacer></v-spacer>
 
-     
+     <div class="d-none d-sm-flex">
+       <v-btn small plain text style="text-transform:none" class="font-3 weight-500 white--text"><span class="mb-n1">Profile</span> <v-avatar size="30" class="ml-2"><v-img eager src="https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"></v-img></v-avatar></v-btn>
+     </div>
         <v-app-bar-nav-icon class="white--text d-flex d-sm-none" />
      
     </v-app-bar>
