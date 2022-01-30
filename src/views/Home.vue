994bf3c4165f5db232523f6954f8fe984243b2a5
Backendpapa <98647710+backendpapa@users.noreply.github.com>
2022-01-30 18:14:47 +0100

    initial disney

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,28 @@
 <template>
-  <hello-world />
+ <div>
+   <div class="bg">
+      <v-container>
+        
+      </v-container>
+   </div>
+ </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+
 
   export default {
     name: 'Home',
 
     components: {
-      HelloWorld,
+    
     },
   }
 </script>
+<style>
+.bg{
+  background: linear-gradient(90deg, rgba(0, 0, 0, 0.025) 8.54%, rgba(0, 0, 0, 0.273638) 51.03%, rgba(0, 0, 0, 0.5) 100%),url('../assets/images/hero2.jpg') ;
+  height: 100vh;
+  background-size: cover;
+}
+</style>
