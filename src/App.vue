a1a73fc004b0f8972f1af6c995cb0530f38f767e
Backendpapa <98647710+backendpapa@users.noreply.github.com>
2022-01-30 21:36:27 +0100

    initial disney

--- src/App.vue
+++ src/App.vue
@@ -12,19 +12,19 @@
 
   <div class="d-none d-sm-flex">
      <div style="margin-left:10%"  class="d-flex align-center">
-     <v-btn  small plain text style="text-transform:none" class="font-3 weight-500 white--text"><v-icon size="18">mdi-home</v-icon> <span class="mb-n1">HOME</span></v-btn>
-      <v-btn small plain text style="text-transform:none" class="font-3 weight-500 white--text"><v-icon size="18">mdi-magnify</v-icon> <span class="mb-n1">SEARCH</span></v-btn>
-       <v-btn small plain text style="text-transform:none" class="font-3 weight-500 white--text"><v-icon size="18">mdi-plus</v-icon> <span class="mb-n1">WATCHLIST</span></v-btn>
-       <v-btn small plain text style="text-transform:none" class="font-3 weight-500 white--text"><v-icon size="18">mdi-star</v-icon> <span class="mb-n1">ORIGINALS</span></v-btn>
-       <v-btn small plain text style="text-transform:none" class="font-3 weight-500 white--text"><v-icon size="18">mdi-movie-star-outline</v-icon> <span class="mb-n1">MOVIES</span></v-btn>
-       <v-btn small plain text style="text-transform:none" class="font-3 weight-500 white--text"><v-icon size="18">mdi-wallet-giftcard</v-icon> <span class="mb-n1">SERIES</span></v-btn>
+     <v-btn  small plain text style="text-transform:none" class="font-head weight-500 white--text"><v-icon size="18">mdi-home</v-icon> <span class="mb-n1">HOME</span></v-btn>
+      <v-btn small plain text style="text-transform:none" class="font-head weight-500 white--text"><v-icon size="18">mdi-magnify</v-icon> <span class="mb-n1">SEARCH</span></v-btn>
+       <v-btn small plain text style="text-transform:none" class="font-head weight-500 white--text"><v-icon size="18">mdi-plus</v-icon> <span class="mb-n1">WATCHLIST</span></v-btn>
+       <v-btn small plain text style="text-transform:none" class="font-head weight-500 white--text"><v-icon size="18">mdi-star</v-icon> <span class="mb-n1">ORIGINALS</span></v-btn>
+       <v-btn small plain text style="text-transform:none" class="font-head weight-500 white--text"><v-icon size="18">mdi-movie-star-outline</v-icon> <span class="mb-n1">MOVIES</span></v-btn>
+       <v-btn small plain text style="text-transform:none" class="font-head weight-500 white--text"><v-icon size="18">mdi-wallet-giftcard</v-icon> <span class="mb-n1">SERIES</span></v-btn>
        
       </div>
   </div>
       <v-spacer></v-spacer>
 
      <div class="d-none d-sm-flex">
-       <v-btn small plain text style="text-transform:none" class="font-3 weight-500 white--text"><span class="mb-n1">Profile</span> <v-avatar size="30" class="ml-2"><v-img eager src="https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"></v-img></v-avatar></v-btn>
+       <v-btn small plain text style="text-transform:none" class="font-head weight-500 white--text"><span class="mb-n1">Profile</span> <v-avatar size="30" class="ml-2"><v-img eager src="https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"></v-img></v-avatar></v-btn>
      </div>
         <v-app-bar-nav-icon class="white--text d-flex d-sm-none" />
      
